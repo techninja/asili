2fe2320a989d4a4a78d2479d16a1f6d317a2698b
James Todd <james@tn42.com>
2026-01-28 13:49:46 -0800

    Clean up empirical fail

--- packages/pipeline/migrations/002_add_empirical_stats.sql
+++
@@ -1,4 +0,0 @@
--- Add empirical_stats column for storing population-level PGS distributions
--- Computed from 1000 Genomes Project reference data
-
-ALTER TABLE traits ADD COLUMN IF NOT EXISTS empirical_stats VARCHAR;
